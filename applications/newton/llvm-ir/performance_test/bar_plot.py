94ab646b457c26f0be0c1ca31e53e5ed6b3a9fb3
PeiMu <ds1231h@gmail.com>
2024-01-15 17:38:39 +0000

    add/remove figure plot scripts
    * master.

---
+++ applications/newton/llvm-ir/performance_test/bar_plot.py
@@ -0,0 +1,217 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+breakdown = True
+
+
+def bar_plot_groups(labels,
+                    y1,
+                    y2,
+                    y1_label,
+                    y2_label,
+                    ylabel,
+                    title,
+                    save_fig=''):
+    """
+    A simple wrapper for matplotlib's barplot.
+
+    :param labels:
+    :param y1:
+    :param y2:
+    :param ylabel:
+    :param xlabel:
+    :param save_fig:
+    :return:
+    """
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.rc('font', size=12)
+
+    fig, ax = plt.subplots()
+    ax.bar(x[1:len(y1)], y1[1:], width, label=y1_label, color='#c2a5cf')
+    ax.bar(x, y2, width, label=y2_label, color='#7b3294')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+
+    plt.xticks(rotation=45)
+    ax.yaxis.grid()  # horizontal lines
+
+    fig.tight_layout()
+
+    if len(save_fig):
+        plt.savefig(save_fig)
+    else:
+        plt.show()
+
+
+def bar_plot_pairs(labels,
+                   y1,
+                   y2,
+                   y1_label,
+                   y2_label,
+                   ylabel,
+                   title,
+                   save_fig=''):
+    """
+    A simple wrapper for matplotlib's barplot.
+
+    :param labels:
+    :param y1:
+    :param y2:
+    :param ylabel:
+    :param xlabel:
+    :param save_fig:
+    :return:
+    """
+    x = np.arange(len(labels))  # the label locations
+    width = 0.35  # the width of the bars
+
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif')
+    plt.rc('font', size=12)
+
+    fig, ax = plt.subplots()
+
+    if breakdown:
+        ax.bar(x[0:4] - width / 2, y1[0:4], width, label=y1_label+"w/o Overload", color='#fdb863')
+        ax.bar(x[0:4] + width / 2, y2[0:4], width, label=y2_label+"w/o Overload", color='#e66101')
+        ax.bar(x[4:] - width / 2, y1[4:], width, label=y1_label+"w/ Overload", color='#c2a4cf')
+        ax.bar(x[4:] + width / 2, y2[4:], width, label=y2_label+"w/ Overload", color='#7b3294')
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        if ylabel == 'Speedup':
+            y_axis_length = [0.9, 2.4]
+            ax.set_ylim(y_axis_length)
+            ax.set_yticks(np.arange(y_axis_length[0]+0.1, y_axis_length[1]+0.1, 0.2))
+        elif ylabel == 'Size reduction (\%)':
+            y_axis_length = [2, 13]
+            ax.set_ylim(y_axis_length)
+            ax.set_yticks(np.arange(y_axis_length[0], y_axis_length[1], 2))
+    else:
+        ax.bar(x - width / 2, y1, width, label=y1_label, color='#c2a4cf')
+        ax.bar(x + width / 2, y2, width, label=y2_label, color='#7b3294')
+
+        # Add some text for labels, title and custom x-axis tick labels, etc.
+        if ylabel == 'Speedup':
+            y_axis_length = [1, 2.1]
+            ax.set_ylim(y_axis_length)
+            ax.set_yticks(np.arange(y_axis_length[0], y_axis_length[1], 0.2))
+        elif ylabel == 'Size reduction (\%)':
+            y_axis_length = [-5, 36]
+            ax.set_ylim(y_axis_length)
+            ax.set_yticks(np.arange(y_axis_length[0], y_axis_length[1], 5))
+
+    ax.set_ylabel(ylabel)
+    ax.set_title(title)
+    ax.set_xticks(x)
+    ax.set_xticklabels(labels)
+    ax.legend(prop={'size': 9})
+    fig.set_figheight(3)
+
+    plt.xticks(rotation=45)
+    ax.yaxis.grid()  # horizontal lines
+
+    fig.tight_layout()
+
+    if len(save_fig) > 0:
+        plt.savefig(save_fig)
+
+
+if __name__ == '__main__':
+    if breakdown:
+        df = pd.read_excel('breakdown_performance.xlsx',
+                           sheet_name='speedup',
+                           nrows=5,
+                           usecols='C:F',
+                           engine='openpyxl')
+        df.rename(columns={
+            "type compression": "Type\nCompression",
+            "branch elimination": "Condition\nSimplification",
+            "constant substitution": "Constant\nSubstitution",
+            "overall": "Overall"
+        },
+                  inplace=True)
+        print(df)
+
+        labels = list(df.columns.values)
+        labels = labels + labels
+
+        y1 = list(df.iloc[0]) + list(df.iloc[1])
+        y2 = list(df.iloc[2]) + list(df.iloc[3])
+
+        bar_plot_pairs(labels, y1, y2, 'arm', 'x86', 'Speedup',
+                       'Speedup breakdown - fp_add [0,200]', 'breakdown_speedup.png')
+
+        df = pd.read_excel('breakdown_performance.xlsx',
+                           sheet_name='size',
+                           nrows=5,
+                           usecols='C:F',
+                           engine='openpyxl')
+        df.rename(columns={
+            "type compression": "Type\nCompression",
+            "branch elimination": "Condition\nSimplification",
+            "constant substitution": "Constant\nSubstitution",
+            "overall": "Overall"
+        },
+                  inplace=True)
+        print(df)
+
+        labels = list(df.columns.values)
+        labels = labels + labels
+
+        y1 = list(map(lambda x: 100*x, list(df.iloc[0]) + list(df.iloc[1])))
+        y2 = list(map(lambda x: 100*x, list(df.iloc[2]) + list(df.iloc[3])))
+
+        bar_plot_pairs(labels, y1, y2, 'arm', 'x86', 'Size reduction (\%)',
+                       'Size reduction breakdown - fp_add [0,200]', 'breakdown_size.png')
+
+        # bar_plot_groups(labels, y1, y2, 'w/o Function Overload',
+        #                 'w/ Function Overload', 'Speedup',
+        #                 'Performance breakdown of CoSense optimizations',
+        #                 'speedup_breakdown.png')
+    else:
+        # X86
+        df = pd.read_excel('overall_speedup_microbenchmarks.xlsx',
+                           sheet_name='x86',
+                           nrows=10,
+                           header=None,
+                           names=['benchmark', 'speedup', 'size'],
+                           usecols='B,C,E',
+                           engine='openpyxl')
+        df['benchmark'] = df['benchmark'].apply(
+            lambda x: x.replace(' with time speed up', ''))
+
+        labels = list(df['benchmark'].values)
+        x86_speedup = list(df['speedup'].values)
+        x86_size = list(df['size'].values)
+        x86_speedup = list(map(lambda x: x + 1.0, x86_speedup))
+        x86_size = list(map(lambda x: x * 100, x86_size))
+
+        # ARM
+        df = pd.read_excel('overall_speedup_microbenchmarks.xlsx',
+                           sheet_name='arm64',
+                           nrows=10,
+                           header=None,
+                           names=['benchmark', 'speedup', 'size'],
+                           usecols='A,B,D',
+                           engine='openpyxl')
+
+        arm_speedup = list(df['speedup'].values)
+        arm_size = list(df['size'].values)
+        arm_speedup = list(map(lambda x: x + 1.0, arm_speedup))
+        arm_size = list(map(lambda x: x * 100, arm_size))
+
+        bar_plot_pairs(labels, arm_speedup, x86_speedup, 'arm', 'x86', 'Speedup',
+                       'Speedup in microbenchmarks', 'speedup_microbenchmarks.png')
+
+        bar_plot_pairs(labels, arm_size, x86_size, 'arm', 'x86',
+                       'Size reduction (\%)', 'Library size reduction in microbenchmarks',
+                       'size_reduction_microbenchmarks.png')
--- applications/newton/llvm-ir/performance_test/plot_sensor_ranges.py
+++ applications/newton/llvm-ir/performance_test/plot_sensor_ranges.py
@@ -45,14 +45,13 @@ import platform
 
 # [instruction counts (million)", "time consumption (s)", "IR lines", "size of library (bytes)"]
 y_units = [1000, 1000, 1, 1]
+# changed with auto_test.cpp
+range_extend_list = [1]
 
-params_num = 10
+params_num = 17
 test_case_num = 10
 merit_num = 4
-range_extend_num = 6
-
-# changed with auto_test.cpp
-range_extend_list = [1, 10, 100, 1000, 10000, 100000]
+range_extend_num = len(range_extend_list)
 
 average_data = []
 with open('average_speedup.log', 'r') as f:
@@ -70,13 +69,13 @@ average_libsize_reduce = []
 for i in range(1, len(average_data), range_extend_num):
     time_speedup = 0
     libsize_reduce = 0
-    for j in range(i, i+range_extend_num):
+    for j in range(i, i + range_extend_num):
         time_speedup += float(average_data[j][3].strip('%')) / 100
         libsize_reduce += float(average_data[j][5].strip('%')) / 100
     average_time_speedup.append(time_speedup / range_extend_num * 100)
     average_libsize_reduce.append(libsize_reduce / range_extend_num * 100)
 
-assert(len(average_name_list) == len(average_time_speedup))
+assert (len(average_name_list) == len(average_time_speedup))
 for i in range(0, len(average_name_list)):
     print(average_name_list[i], "with time speed up: ", format(average_time_speedup[i], '.2f'),
           "%, lib size reduce: ", format(average_libsize_reduce[i], '.2f'), "%")
@@ -98,7 +97,7 @@ for i in range(2, len(performance_data), 3):
 
 param_list = []
 for i in range(1, 3 * params_num * range_extend_num, 3):
-    lower, upper = map(float, performance_data[i][1].split( ))
+    lower, upper = map(float, performance_data[i][1].split())
     lower_r1 = round(lower, 1)
     upper_r1 = round(upper, 1)
     param_list.append('[' + str(lower_r1) + ', ' + str(upper_r1) + ']')
@@ -142,7 +141,7 @@ inst_speedup = np.reshape(inst_speedup, (test_case_num, range_extend_num, params
 
 time_speedup = []
 for i in range(3, len(performance_data), 3):
-    time_speedup.append(float(performance_data[i][3].strip('%')) / 100)
+    time_speedup.append(float(performance_data[i][3].strip('%')) / 100 + 1)
 time_speedup = np.reshape(time_speedup, (test_case_num, range_extend_num, params_num))
 
 ir_reduction = []
@@ -157,7 +156,7 @@ lib_size_reduction = np.reshape(lib_size_reduction, (test_case_num, range_extend
 
 perf_data_speedup = [inst_speedup, time_speedup, ir_reduction, lib_size_reduction]
 
-y_labels = ["instruction counts (million)", "time (s)", "IR lines", "size (bytes)"]
+y_labels = ["instruction counts (million)", "time consumption speedup", "IR lines", "library size reduction ratio"]
 
 machine = platform.machine()
 # machine = "aarch64"
@@ -168,63 +167,34 @@ if folder:
     shutil.rmtree(fig_path, ignore_errors=True)
 os.mkdir(fig_path)
 
-# Histogram
-# for merit_id in range(merit_num):
-#     for test_case_id in range(test_case_num):
-#         for range_extend_id in range(range_extend_num):
-#             ax1 = plt.figure(num= merit_id * test_case_num * range_extend_num +
-#                              test_case_id * range_extend_num + range_extend_id + 1,
-#                              dpi=300)
-#
-#             x = list(range(params_num))
-#             total_width, n = 0.5, 2
-#             width = total_width / n
-#
-#             plt_y_begin = params_num * (test_case_id * range_extend_num + range_extend_id)
-#             plt_y_end = plt_y_begin + params_num
-#             y_ranges = (min(opt_perf_data[merit_id][plt_y_begin:plt_y_end]) * 0.8,
-#                         max(ori_perf_data[merit_id][plt_y_begin:plt_y_end]) * 1.2)
-#
-#             plt.ylim(y_ranges)
-#             plt.ylabel(y_labels[merit_id])
-#
-#             plt.bar(x, ori_perf_data[merit_id][plt_y_begin:plt_y_end],
-#                     width=width, label='basic performance', fc='y')
-#             for i in range(len(x)):
-#                 x[i] = x[i] + width
-#             plt.bar(x, opt_perf_data[merit_id][plt_y_begin:plt_y_end],
-#                     width=width, label='optimized performance', fc='r')
-#             plt.legend()
-#             file_name = fig_path + name_list[test_case_id * range_extend_num + range_extend_id] + "-" + \
-#                         str(range_extend_list[range_extend_id]) + "-" + y_labels[merit_id] + ".pdf"
-#             file_name = file_name.replace(" ", "_")
-#             plt.savefig(file_name)
-#             plt.close()
-#             if test_case_id == test_case_num - 1:
-#                 break
-
+heatmap_fmt = ['.2f', '.2f', '.0%', '.0%']
 # Heatmap
 for merit_id in range(1, merit_num, 2):
-    for test_case_id in range(test_case_num):
-        plt.clf()
-        plt.figure(num=merit_id * test_case_num + test_case_id, dpi=300,
-                   constrained_layout=True)
-        fmt = lambda x, pos: '{:.0%}'.format(x)
-        fig = sns.heatmap(data=perf_data_speedup[merit_id][test_case_id].T,
-                          cmap=plt.get_cmap('Greens'),
-                          annot=True,
-                          fmt=".0%",
-                          cbar_kws={'format': FuncFormatter(fmt)},
-                          xticklabels=range_extend_list,
-                          yticklabels=param_list[0:10])
-        fig.set_xlabel('Range Extent')
-        fig.set_ylabel('Function Parameters')
-        plt.title(machine + "-" + name_list[test_case_id * range_extend_num] + "-" + y_labels[merit_id])
-        file_name = fig_path + machine + "-" + name_list[test_case_id * range_extend_num] + "-" + y_labels[
-            merit_id] + ".png"
-        file_name = file_name.replace(" ", "_")
-        plt.savefig(file_name)
-        plt.close()
+    plt.clf()
+    plt.figure(dpi=300, constrained_layout=True)
+    # for test_case_id in range(test_case_num):
+    cbar_fmt = '{:' + heatmap_fmt[merit_id] + '}'
+    fmt = lambda x, pos: cbar_fmt.format(x)
+    fig = sns.heatmap(data=perf_data_speedup[merit_id].reshape([10, 17]).T,
+                      cmap=plt.get_cmap('Purples'),
+                      annot=True,
+                      fmt=heatmap_fmt[merit_id],
+                      cbar_kws={'format': FuncFormatter(fmt)},
+                      xticklabels=name_list,
+                      yticklabels=param_list[0:params_num])
+    # fig.set_xlabel('Test Cases')
+    fig.set_ylabel('Function Parameters')
+    if machine == "aarch64":
+        machine_name = "ARM (dev-board)"
+    else:
+        machine_name = "x86 (laptop)"
+    plt.title(machine_name + "-" + y_labels[merit_id])
+    plt.xticks(rotation=45)
+    file_name = fig_path + machine + "-" + y_labels[merit_id] + ".png"
+    file_name = file_name.replace(" ", "_")
+    plt.savefig(file_name)
+
+plt.close()
 
 os.system('cp perf.log fig/' + machine + "_perf.log")
 os.system('cp average_speedup.log fig/' + machine + "_average_speedup.log")
