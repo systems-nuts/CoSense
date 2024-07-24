8caeff3e473a731bc92f6f2ce2410f4180b5c163
xyf <2316639449@qq.com>
2024-07-24 09:30:09 +0100

    save before handle constant operation
    * dev2.

--- src/newton/newton-irPass-LLVMIR-dequantization.cpp
+++ src/newton/newton-irPass-LLVMIR-dequantization.cpp
@@ -8,6 +8,135 @@ using namespace llvm;
 
 extern "C" {
 
+// Handle load and store instructions
+void
+handleLoadStoreInstructions(Function & llvmIrFunction, Type * floatType)
+{
+	llvm::errs() << "Entering handleLoadStoreInstructions\n";
+	for (BasicBlock & llvmIrBasicBlock : llvmIrFunction)
+	{
+		for (BasicBlock::iterator itBB = llvmIrBasicBlock.begin(); itBB != llvmIrBasicBlock.end();)
+		{
+			Instruction * llvmIrInstruction = &*itBB++;
+			if (llvmIrInstruction->getOpcode() == Instruction::Load)
+			{
+				if (auto loadInst = dyn_cast<LoadInst>(llvmIrInstruction))
+				{
+					auto ptr = loadInst->getPointerOperand();
+					if (isQuantizedType(ptr->getType()->getPointerElementType()))
+					{
+						ptr->mutateType(floatType->getPointerTo());
+					}
+				}
+			}
+			else if (llvmIrInstruction->getOpcode() == Instruction::Store)
+			{
+				if (auto storeInst = dyn_cast<StoreInst>(llvmIrInstruction))
+				{
+					auto ptr = storeInst->getPointerOperand();
+					if (isQuantizedType(ptr->getType()->getPointerElementType()))
+					{
+						ptr->mutateType(floatType->getPointerTo());
+					}
+				}
+			}
+		}
+	}
+	llvm::errs() << "Exiting handleLoadStoreInstructions\n";
+}
+
+// Dequantize constants within an instruction
+void
+dequantizeConstant(Instruction * inInstruction, Type * floatType)
+{
+	llvm::errs() << "Entering dequantizeConstant\n";
+	for (size_t idx = 0; idx < inInstruction->getNumOperands(); idx++)
+	{
+		Value * inValue = inInstruction->getOperand(idx);
+
+		if (!isa<ConstantInt>(inValue))
+		{
+			continue;
+		}
+
+		auto *	constInt = dyn_cast<ConstantInt>(inValue);
+		Value * newValue = nullptr;
+
+		if (inValue->getType()->isIntegerTy())
+		{
+			// Convert fixed-point to float
+			int64_t fixedPointValue = constInt->getSExtValue();
+			double	floatValue	= static_cast<double>(fixedPointValue) / FRAC_BASE;
+			newValue		= ConstantFP::get(floatType, floatValue);
+		}
+
+		if (newValue)
+		{
+			inInstruction->replaceUsesOfWith(inValue, newValue);
+		}
+	}
+	llvm::errs() << "Exiting dequantizeConstant\n";
+}
+
+// Handle floating-point instructions and dequantize them
+void
+handleFPInstructions(Instruction * inInstruction, Type * floatType)
+{
+	llvm::errs() << "Entering handleFPInstructions\n";
+	IRBuilder<> Builder(inInstruction);
+	switch (inInstruction->getOpcode())
+	{
+		case Instruction::Add:
+		case Instruction::Sub:
+		case Instruction::Mul:
+		case Instruction::UDiv:
+		case Instruction::SDiv:
+		case Instruction::URem:
+		case Instruction::SRem:
+		{
+			Value * lhs = inInstruction->getOperand(0);
+			Value * rhs = inInstruction->getOperand(1);
+			if (isQuantizedType(lhs->getType()))
+			{
+				// Convert integer to float
+				lhs = Builder.CreateSIToFP(lhs, floatType);
+			}
+			if (isQuantizedType(rhs->getType()))
+			{
+				rhs = Builder.CreateSIToFP(rhs, floatType);
+			}
+			Instruction * newInst = nullptr;
+			switch (inInstruction->getOpcode())
+			{
+				case Instruction::Add:
+					newInst = BinaryOperator::CreateFAdd(lhs, rhs, "", inInstruction);
+					break;
+				case Instruction::Sub:
+					newInst = BinaryOperator::CreateFSub(lhs, rhs, "", inInstruction);
+					break;
+				case Instruction::Mul:
+					newInst = BinaryOperator::CreateFMul(lhs, rhs, "", inInstruction);
+					break;
+				case Instruction::UDiv:
+				case Instruction::SDiv:
+					newInst = BinaryOperator::CreateFDiv(lhs, rhs, "", inInstruction);
+					break;
+				case Instruction::URem:
+				case Instruction::SRem:
+					newInst = BinaryOperator::CreateFRem(lhs, rhs, "", inInstruction);
+					break;
+			}
+			inInstruction->replaceAllUsesWith(newInst);
+			inInstruction->eraseFromParent();
+			break;
+		}
+		default:
+			llvm::errs() << "Unhandled instruction: " << *inInstruction << "\n";
+			break;
+	}
+	llvm::errs() << "Exiting handleFPInstructions\n";
+}
+
 // Create a dequantized function with the same signature as the original function
 Function *
 createDequantizedFunction(Function & llvmIrFunction, Type * floatType)
@@ -17,11 +146,13 @@ createDequantizedFunction(Function & llvmIrFunction, Type * floatType)
 	{
 		if (isQuantizedType(arg.getType()))
 		{
+			// Change quantized type to float
 			params.push_back(floatType);
 			llvm::errs() << "Dequantizing parameter: " << arg.getName() << " from " << *arg.getType() << " to " << *floatType << "\n";
 		}
 		else
 		{
+			// Keep original type
 			params.push_back(arg.getType());
 		}
 	}
@@ -66,6 +197,7 @@ replaceFunctionUses(Function & oldFunc, Function * newFunc)
 			newCall->setDebugLoc(callInst->getDebugLoc());
 			llvm::errs() << "Replacing call: " << *callInst << " with " << *newCall << "\n";
 			callInst->replaceAllUsesWith(newCall);
+			callInst->eraseFromParent();
 			callInst->eraseFromParent();
 		}
 	}
@@ -120,32 +252,43 @@ setDequantizedType(Value * inValue, Type * floatType)
 }
 
 // Adapt type casts to the dequantized type
-void adaptTypeCast(Function &llvmIrFunction, Type *floatType) {
+void
+adaptTypeCast(Function & llvmIrFunction, Type * floatType)
+{
 	llvm::errs() << "Entering adaptTypeCast\n";
-	for (BasicBlock &llvmIrBasicBlock : llvmIrFunction) {
-		for (BasicBlock::iterator itBB = llvmIrBasicBlock.begin(); itBB != llvmIrBasicBlock.end();) {
-			Instruction *llvmIrInstruction = &*itBB++;
-			switch (llvmIrInstruction->getOpcode()) {
+	for (BasicBlock & llvmIrBasicBlock : llvmIrFunction)
+	{
+		for (BasicBlock::iterator itBB = llvmIrBasicBlock.begin(); itBB != llvmIrBasicBlock.end();)
+		{
+			Instruction * llvmIrInstruction = &*itBB++;
+			switch (llvmIrInstruction->getOpcode())
+			{
 				case Instruction::FPToUI:
 				case Instruction::FPToSI:
 				case Instruction::UIToFP:
 				case Instruction::SIToFP:
 				{
 					IRBuilder<> Builder(llvmIrInstruction);
-					Value *operand = llvmIrInstruction->getOperand(0);
-					Value *newInst = nullptr;
+					Value *	    operand = llvmIrInstruction->getOperand(0);
+					Value *	    newInst = nullptr;
 
-					if (llvmIrInstruction->getOpcode() == Instruction::FPToUI || llvmIrInstruction->getOpcode() == Instruction::FPToSI) {
-						if (isQuantizedType(operand->getType())) {
+					if (llvmIrInstruction->getOpcode() == Instruction::FPToUI || llvmIrInstruction->getOpcode() == Instruction::FPToSI)
+					{
+						if (isQuantizedType(operand->getType()))
+						{
 							newInst = Builder.CreateSIToFP(operand, floatType);
 						}
-					} else if (llvmIrInstruction->getOpcode() == Instruction::UIToFP || llvmIrInstruction->getOpcode() == Instruction::SIToFP) {
-						if (isQuantizedType(llvmIrInstruction->getType())) {
+					}
+					else if (llvmIrInstruction->getOpcode() == Instruction::UIToFP || llvmIrInstruction->getOpcode() == Instruction::SIToFP)
+					{
+						if (isQuantizedType(llvmIrInstruction->getType()))
+						{
 							newInst = Builder.CreateSIToFP(operand, floatType);
 						}
 					}
 
-					if (newInst) {
+					if (newInst)
+					{
 						llvmIrInstruction->replaceAllUsesWith(newInst);
 						llvmIrInstruction->eraseFromParent();
 					}
@@ -155,16 +298,20 @@ void adaptTypeCast(Function &llvmIrFunction, Type *floatType) {
 				case Instruction::FPTrunc:
 				{
 					IRBuilder<> Builder(llvmIrInstruction);
-					Value *operand = llvmIrInstruction->getOperand(0);
-					Value *newInst = nullptr;
+					Value *	    operand = llvmIrInstruction->getOperand(0);
+					Value *	    newInst = nullptr;
 
-					if (isQuantizedType(operand->getType())) {
+					if (isQuantizedType(operand->getType()))
+					{
 						newInst = Builder.CreateSIToFP(operand, floatType);
-					} else {
+					}
+					else
+					{
 						newInst = Builder.CreateFPCast(operand, floatType);
 					}
 
-					if (newInst) {
+					if (newInst)
+					{
 						llvmIrInstruction->replaceAllUsesWith(newInst);
 						llvmIrInstruction->eraseFromParent();
 					}
@@ -173,10 +320,11 @@ void adaptTypeCast(Function &llvmIrFunction, Type *floatType) {
 				case Instruction::BitCast:
 				{
 					IRBuilder<> Builder(llvmIrInstruction);
-					Value *operand = llvmIrInstruction->getOperand(0);
-					Value *newInst = Builder.CreateBitCast(operand, floatType);
+					Value *	    operand = llvmIrInstruction->getOperand(0);
+					Value *	    newInst = Builder.CreateBitCast(operand, floatType);
 
-					if (newInst) {
+					if (newInst)
+					{
 						llvmIrInstruction->replaceAllUsesWith(newInst);
 						llvmIrInstruction->eraseFromParent();
 					}
@@ -213,5 +361,26 @@ irPassLLVMIRAutoDequantization(State * N, llvm::Function & llvmIrFunction, std::
 	llvm::errs() << "Finished handling function signature for: " << newFunc->getName() << "\n";
 
 	handleLoadStoreInstructions(llvmIrFunction, floatType);
+
+	for (BasicBlock & BB : llvmIrFunction)
+	{
+		for (BasicBlock::iterator it = BB.begin(); it != BB.end();)
+		{
+			Instruction * I = &*it++;
+			if (isQuantizedType(I->getType()))
+			{
+				setDequantizedType(I, floatType);
+			}
+			else
+			{
+				handleFPInstructions(I, floatType);
+				dequantizeConstant(I, floatType);
+			}
+		}
+	}
+
+	adaptTypeCast(llvmIrFunction, floatType);
+
 	llvm::errs() << "Exiting irPassLLVMIRAutoDequantization\n";
+}
 }
