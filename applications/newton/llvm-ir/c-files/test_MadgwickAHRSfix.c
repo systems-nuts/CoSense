aa88e2d6c3c11fb59f0d4c70b57122722b202729
xyf <2316639449@qq.com>
2024-08-17 13:33:49 +0800

    add more test files
    * dev2.

---
+++ applications/newton/llvm-ir/c-files/test_MadgwickAHRSfix.c
@@ -0,0 +1,162 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#define FRAC_Q 10
+#define BIT_WIDTH 32
+#define ITERATION 1
+#include "MadgwickAHRSfix.h"
+extern volatile int32_t q0, q1, q2, q3;
+extern void MadgwickAHRSupdate(int32_t gx, int32_t gy, int32_t gz, int32_t ax, int32_t ay, int32_t az, int32_t mx, int32_t my, int32_t mz, int32_t * q0_ptr, int32_t * q1_ptr, int32_t * q2_ptr, int32_t * q3_ptr);
+extern void MadgwickAHRSupdateIMU(int32_t gx, int32_t gy, int32_t gz, int32_t ax, int32_t ay, int32_t az, int32_t * q0_ptr, int32_t * q1_ptr, int32_t * q2_ptr, int32_t * q3_ptr);
+
+// #include "MadgwickAHRS.h"
+
+/***************************************
+ * Timer functions of the test framework
+ ***************************************/
+
+typedef struct timespec timespec;
+timespec
+diff(timespec start, timespec end)
+{
+	timespec temp;
+	if ((end.tv_nsec - start.tv_nsec) < 0)
+	{
+		temp.tv_sec  = end.tv_sec - start.tv_sec - 1;
+		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+	}
+	else
+	{
+		temp.tv_sec  = end.tv_sec - start.tv_sec;
+		temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+	}
+	return temp;
+}
+
+timespec
+sum(timespec t1, timespec t2)
+{
+	timespec temp;
+	if (t1.tv_nsec + t2.tv_nsec >= 1000000000)
+	{
+		temp.tv_sec  = t1.tv_sec + t2.tv_sec + 1;
+		temp.tv_nsec = t1.tv_nsec + t2.tv_nsec - 1000000000;
+	}
+	else
+	{
+		temp.tv_sec  = t1.tv_sec + t2.tv_sec;
+		temp.tv_nsec = t1.tv_nsec + t2.tv_nsec;
+	}
+	return temp;
+}
+
+void
+printTimeSpec(timespec t, const char * prefix)
+{
+	printf("%s: %d.%09d\n", prefix, (int)t.tv_sec, (int)t.tv_nsec);
+}
+
+// Quantization function
+int
+quantize(float value, int frac_base)
+{
+	return round(value * frac_base);
+}
+
+// Dequantization function
+float
+dequantize(int quantized_value)
+{
+	return (float)(quantized_value >> FRAC_Q);
+}
+
+extern int perform_addition(float a, float b);
+
+timespec
+tic()
+{
+
+	timespec start_time;
+	clock_gettime(CLOCK_REALTIME, &start_time);
+	return start_time;
+}
+
+timespec
+toc(timespec * start_time, const char * prefix)
+{
+	timespec current_time;
+	clock_gettime(CLOCK_REALTIME, &current_time);
+	timespec time_consump = diff(*start_time, current_time);
+	printTimeSpec(time_consump, prefix);
+	*start_time = current_time;
+	return time_consump;
+}
+int
+main()
+{
+	/*
+	 * Time (s),Magnetic field x (µT),Magnetic field y (µT),Magnetic field z (µT),Gyroscope x (rad/s),Gyroscope y (rad/s),Gyroscope z (rad/s),Acceleration x (m/s^2),Acceleration y (m/s^2),Acceleration z (m/s^2)
+0.07969094,-7.249095917,26.43893433,-37.16656494,-0.1184487343,0.001258035656,0.008988874033,-0.3570917249,0.3941296637,9.963726044
+	 */
+
+	float num1 = -7.249095917;
+	float num2 = 26.43893433;
+	float num3 = -37.16656494;
+	float num4 = -0.1184487343;
+	float num5 = 0.001258035656;
+	float num6 = 0.008988874033;
+	float num7 = -0.3570917249;
+	float num8 = 0.3941296637;
+	float num9 = 9.963726044;
+
+	float num10 = 0.64306622;
+	float num11 = 0.02828862;
+	float num12 = -0.00567953;
+	float num13 = -0.76526684;
+
+	int32_t mag_x = quantize(num1, FRAC_BASE);
+	int32_t mag_y = quantize(num2, FRAC_BASE);
+	int32_t mag_z = quantize(num3, FRAC_BASE);
+	int32_t gyr_x = quantize(num4, FRAC_BASE);
+	int32_t gyr_y = quantize(num5, FRAC_BASE);
+	int32_t gyr_z = quantize(num6, FRAC_BASE);
+	int32_t acc_x = quantize(num7, FRAC_BASE);
+	int32_t acc_y = quantize(num8, FRAC_BASE);
+	int32_t acc_z = quantize(num9, FRAC_BASE);
+	int32_t q0 = quantize(num10, FRAC_BASE);
+	int32_t q1 = quantize(num11, FRAC_BASE);
+	int32_t q2 = quantize(num12, FRAC_BASE);
+	int32_t q3 = quantize(num13, FRAC_BASE);
+
+	u_int64_t time_slots[ITERATION];
+
+	for (size_t idx = 0; idx < ITERATION; idx++)
+	{
+		timespec timer = tic();
+		// for (size_t ts = 0; ts < DATA_SIZE; ts++)
+		//{
+//		MadgwickAHRSupdate(gyr_x[ts], gyr_y[ts], gyr_z[ts],
+//				   acc_x[ts], acc_y[ts], acc_z[ts],
+//				   mag_x[ts], mag_y[ts], mag_z[ts],
+//				   &q0[ts], &q1[ts], &q2[ts], &q3[ts]);
+		//}
+
+		MadgwickAHRSupdate(gyr_x, gyr_y, gyr_z,
+				   acc_x, acc_y, acc_z,
+				   mag_x, mag_y, mag_z,
+				   &q0, &q1, &q2, &q3);
+		time_slots[idx] = toc(&timer, "computation delay").tv_nsec;
+	}
+
+	u_int64_t average_time = 0;
+	for (size_t idx = 0; idx < ITERATION; idx++)
+	{
+		average_time += time_slots[idx];
+	}
+	average_time /= ITERATION;
+	printf("average time = %lu nm\n", average_time);
+}
---
+++ applications/newton/llvm-ir/testMadgwickfix.sh
@@ -0,0 +1,24 @@
+# Step 1: Generate LLVM IR file
+clang -g -O0 -Xclang -disable-O0-optnone -S -emit-llvm -Wall -Wextra -o /home/xyf/CoSense/applications/newton/llvm-ir/MadgwickAHRSfix.ll /home/xyf/CoSense/applications/newton/llvm-ir/c-files/MadgwickAHRSfix.c
+
+
+# Step 4: Optimize the generated LLVM IR file
+opt /home/xyf/CoSense/applications/newton/llvm-ir/MadgwickAHRSfix.ll --simplifycfg --instsimplify -O3 -Os -S -o /home/xyf/CoSense/applications/newton/llvm-ir/out.llperformace
+
+# Step 5: Compile the optimized LLVM IR file to bitcode
+llvm-as /home/xyf/CoSense/applications/newton/llvm-ir/out.llperformace -o /home/xyf/CoSense/applications/newton/llvm-ir/out.bc
+
+# Step 6: Compile the bitcode file to assembly
+llc /home/xyf/CoSense/applications/newton/llvm-ir/out.bc -o /home/xyf/CoSense/applications/newton/llvm-ir/out.s
+
+# Step 7: Compile the assembly file to object file
+clang -c /home/xyf/CoSense/applications/newton/llvm-ir/out.s -o /home/xyf/CoSense/applications/newton/llvm-ir/out.o
+
+# Step 8: Package the object file into a static library
+ar -rc /home/xyf/CoSense/applications/newton/llvm-ir/libout.a /home/xyf/CoSense/applications/newton/llvm-ir/out.o
+
+# Step 9: Compile the test file and link with the static library
+clang /home/xyf/CoSense/applications/newton/llvm-ir/c-files/test_MadgwickAHRSfix.c -no-pie -L/home/xyf/CoSense/applications/newton/llvm-ir -lout -O3 -Os -g -fno-builtin -o /home/xyf/CoSense/applications/newton/llvm-ir/main_out -lm
+
+# Step 10: Run the test executable
+/home/xyf/CoSense/applications/newton/llvm-ir/main_out
