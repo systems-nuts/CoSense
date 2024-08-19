d75c826fb59c52307c5b9b3dec1480b538830566
xyf <2316639449@qq.com>
2024-08-19 22:54:33 +0800

    update test script
    * dev2.

--- applications/newton/llvm-ir/testOriginal.sh
+++ applications/newton/llvm-ir/testOriginal.sh
@@ -1,24 +1,27 @@
+USER_HOME=$HOME
+
+
 # Step 1: Generate LLVM IR file
-clang -g -O0 -Xclang -disable-O0-optnone -S -emit-llvm -Wall -Wextra -o /home/xyf/CoSense/applications/newton/llvm-ir/MadgwickAHRS.ll /home/xyf/CoSense/applications/newton/llvm-ir/c-files/MadgwickAHRS.c
+clang -g -O0 -Xclang -disable-O0-optnone -S -emit-llvm -Wall -Wextra -o $HOME/CoSense/applications/newton/llvm-ir/MadgwickAHRS.ll $HOME/CoSense/applications/newton/llvm-ir/c-files/MadgwickAHRS.c
 
 
 # Step 4: Optimize the generated LLVM IR file
-opt /home/xyf/CoSense/applications/newton/llvm-ir/MadgwickAHRS.ll --simplifycfg --instsimplify -O3 -Os -S -o /home/xyf/CoSense/applications/newton/llvm-ir/out.llperformace
+opt $HOME/CoSense/applications/newton/llvm-ir/MadgwickAHRS.ll --simplifycfg --instsimplify -O3 -Os -S -o $HOME/CoSense/applications/newton/llvm-ir/out.llperformace
 
 # Step 5: Compile the optimized LLVM IR file to bitcode
-llvm-as /home/xyf/CoSense/applications/newton/llvm-ir/out.llperformace -o /home/xyf/CoSense/applications/newton/llvm-ir/out.bc
+llvm-as $HOME/CoSense/applications/newton/llvm-ir/out.llperformace -o $HOME/CoSense/applications/newton/llvm-ir/out.bc
 
 # Step 6: Compile the bitcode file to assembly
-llc /home/xyf/CoSense/applications/newton/llvm-ir/out.bc -o /home/xyf/CoSense/applications/newton/llvm-ir/out.s
+llc $HOME/CoSense/applications/newton/llvm-ir/out.bc -o $HOME/CoSense/applications/newton/llvm-ir/out.s
 
 # Step 7: Compile the assembly file to object file
-clang -c /home/xyf/CoSense/applications/newton/llvm-ir/out.s -o /home/xyf/CoSense/applications/newton/llvm-ir/out.o
+clang -c $HOME/CoSense/applications/newton/llvm-ir/out.s -o $HOME/CoSense/applications/newton/llvm-ir/out.o
 
 # Step 8: Package the object file into a static library
-ar -rc /home/xyf/CoSense/applications/newton/llvm-ir/libout.a /home/xyf/CoSense/applications/newton/llvm-ir/out.o
+ar -rc $HOME/CoSense/applications/newton/llvm-ir/libout.a $HOME/CoSense/applications/newton/llvm-ir/out.o
 
 # Step 9: Compile the test file and link with the static library
-clang /home/xyf/CoSense/applications/newton/llvm-ir/c-files/test_Original.c -no-pie -L/home/xyf/CoSense/applications/newton/llvm-ir -lout -O3 -Os -g -fno-builtin -o /home/xyf/CoSense/applications/newton/llvm-ir/main_out -lm
+clang $HOME/CoSense/applications/newton/llvm-ir/c-files/test_Original.c -no-pie -L$HOME/CoSense/applications/newton/llvm-ir -lout -O3 -Os -g -fno-builtin -o $HOME/CoSense/applications/newton/llvm-ir/main_out -lm
 
 # Step 10: Run the test executable
-/home/xyf/CoSense/applications/newton/llvm-ir/main_out
+$HOME/CoSense/applications/newton/llvm-ir/main_out
